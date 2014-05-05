decdc2dbb72719bc29c9a510b9ca74a479538f3d
Arcen1k <jkahn@universityprep.org>
2014-05-05 11:47:22 -0700

    More updates

--- src/BuffonsNeedle.java
+++ src/BuffonsNeedle.java
@@ -4,19 +4,23 @@ public class BuffonsNeedle {
 	public static void main(String[] args) {		
 		
 		int l = 1, d = 1; // Define length of needle and distance
-		int w = 3 *d, h = d;
+		int w = 2 * d, h = d;
+		
+		int yes = 0, no = 0;
 		
 		boolean running = true;
 		
 		Center c = (x,y,theta) -> {return new Point(x,y,theta);};
 		
-		new Thread(
-				() -> {
-					while (running == true) {
-						System.out.println(running);
-					}
+		Runnable r = new Runnable() {
+			@Override
+			public void run() {
+				while (running) {
+					Point t = c.center(Math.random() * (double) d, Math.random() * (double) h, Math.random() * 180D);
+					if ()
 				}
-		).start();
+			}
+		}
 
 	}
 	
