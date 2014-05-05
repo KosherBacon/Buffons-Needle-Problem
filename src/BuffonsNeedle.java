faa85f343bcae953970638132be2512be0943039
Arcen1k <jkahn@universityprep.org>
2014-05-05 11:37:45 -0700

    More setup

--- src/BuffonsNeedle.java
+++ src/BuffonsNeedle.java
@@ -1,9 +1,39 @@
 
 public class BuffonsNeedle {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {		
+		
+		int l = 1, d = 1; // Define length of needle and distance
+		int w = 3 *d, h = d;
+		
+		boolean running = true;
+		
+		Center c = (x,y,theta) -> {return new Point(x,y,theta);};
+		
+		new Thread(
+				() -> {
+					while (running == true) {
+						System.out.println(running);
+					}
+				}
+		).start();
 
 	}
+	
+	interface Center {
+		Point center(double x, double y, double theta);
+	}
+	
+}
 
+class Point {
+	
+	double x,y,theta;
+	
+	public Point(double x, double y, double theta) {
+		this.x = x;
+		this.y = y;
+		this.theta = theta;
+	}
+	
 }
